e631781b60bb9b271125857251c09254a9626bdf
nguyenkhang011204 <nguyenkhang011204@gmail.com>
2026-03-03 23:03:20 +0700

    [SW3]: Initialize project structure (#1)
    * Initialize project structure
    
    * Adding flex property into footer

---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="/shopping-website-php/assets/css/header.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <header class="header">
+        <div class="container header-wrapper">
+            <!-- Logo -->
+            <div class="logo">
+                <a href="/shopping-website-php/index.php"><img src="/shopping-website-php/assets/images/logo.png" alt="HKT Logo"></a>
+            </div>
+
+            <!-- Search -->
+            <div class="search-box">
+                <form action="" method="GET">
+                    <div class="search-input">
+                        <input type="text" name="keyword" placeholder="Tìm kiếm ...">
+                        <button type="submit">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+            <!-- Icons -->
+            <div class="header-icons">
+                <a href="#"><i class="fa fa-user"></i></a>
+
+                <a href="#" class="cart">
+                    <i class="fa fa-shopping-bag"></i>
+                    <span class="cart-count">0</span>
+                </a>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
---
+++ includes/navbar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/shopping-website-php/assets/css/navbar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <nav class="navbar">
+        <ul>
+            <li><a href="#">TRANG CHỦ</a></li>
+            <li class="dropdown">
+                <a href="#">SẢN PHẨM ▾</a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Áo</a></li>
+                    <li><a href="#">Quần</a></li>
+                    <li><a href="#">Phụ kiện</a></li>
+                </ul>
+            </li>
+            <li><a href="#">NAM</a></li>
+            <li><a href="#">NỮ</a></li>
+            <li><a href="#">VỀ CHÚNG TÔI</a></li>
+            <li><a href="#">ĐƠN HÀNG</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
