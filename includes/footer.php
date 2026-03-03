0425dc03bf0119c089e1adb24168babc65aec56a
Nguyen Duy Khang <nguyenkhang011204@gmail.com>
2026-03-03 22:30:02 +0700

    Initialize project structure

---
+++ includes/footer.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/shopping-website-php/assets/css/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <footer class="footer">
+
+        <!-- Footer Top -->
+        <div class="footer-top">
+            <div class="container footer-wrapper">
+
+                <!-- Cột 1 -->
+                <div class="footer-col">
+                    <h3>Thông tin shop</h3>
+                    <p>HKT Shop</p>
+                    <p>Địa chỉ: 123 Nguyễn Trãi, TP.HCM</p>
+                    <p>Hotline: 0909 xxx xxx</p>
+                    <p>Email: hkshop@gmail.com</p>
+                </div>
+
+                <!-- Cột 2 -->
+                <div class="footer-col">
+                    <h3>Chính sách mua hàng</h3>
+                    <p>Hướng dẫn bảo quản</p>
+                    <p>Chính sách đổi trả và bảo hành</p>
+                    <p>Chính sách bảo mật thông tin</p>
+                    <p>Cách vận chuyển và kiểm tra hàng</p>
+                </div>
+
+                <!-- Cột 3 -->
+                <div class="footer-col">
+                    <h3>Kết nối với chúng tôi</h3>
+                    <div class="social-icons">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-facebook-messenger"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <!-- Footer Bottom -->
+        <div class="footer-bottom">
+            <p>© 2026 HKT Shop. All rights reserved.</p>
+        </div>
+
+    </footer>
+</body>
+</html>
