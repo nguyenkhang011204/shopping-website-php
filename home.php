3d829f49f5f2eedb5d15330b2a9bf0eba7bc7ca5
Nguyen Duy Khang <nguyenkhang011204@gmail.com>
2026-03-07 17:53:36 +0700

    [SW9]: Build product page (product.php) (temporary)

--- home.php
+++ home.php
@@ -10,7 +10,6 @@
 <body>
     <?php include_once('includes/header.php'); ?>
     <?php include_once('includes/navbar.php'); ?>
-
     <main class="content">
 
         <div class="carousel">
---
+++ pages/product.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/shopping-website-php/assets/css/product.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php include_once('../includes/header.php');?>
+    <?php include_once('../includes/navbar.php');?>
+    <main class="content">
+        <section class="product-header">
+
+            <div class="product-header-top">
+                <h2>TẤT CẢ SẢN PHẨM</h2>
+
+                <div class="sort">
+                    <label>SẮP XẾP THEO:</label>
+                    <select>
+                        <option>Mặc định</option>
+                        <option>Giá thấp → cao</option>
+                        <option>Giá cao → thấp</option>
+                        <option>Mới nhất</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="line"></div>
+
+        </section>
+
+        <section class="product-list">    
+    
+            <div class="product-item">
+                <img src="/shopping-website-php/upload/Ao321.png" alt="">
+                <h3>Áo thể thao HKT - Sport T-Shirt</h3>
+                <p class="price">379.000đ</p>
+            </div>
+
+            <div class="product-item">
+                <img src="/shopping-website-php/upload/Ao321.png" alt="">
+                <h3>Áo thể thao HKT - Sport T-Shirt</h3>
+                <p class="price">379.000đ</p>
+            </div>
+
+            <div class="product-item">
+                <img src="/shopping-website-php/upload/Ao321.png" alt="">
+                <h3>Áo thể thao HKT - Sport T-Shirt</h3>
+                <p class="price">379.000đ</p>
+            </div>
+
+            <div class="product-item">
+                <img src="/shopping-website-php/upload/Ao321.png" alt="">
+                <h3>Áo thể thao HKT - Sport T-Shirt</h3>
+                <p class="price">379.000đ</p>
+            </div>
+
+            <div class="product-item">
+                <img src="/shopping-website-php/upload/Ao321.png" alt="">
+                <h3>Áo thể thao HKT - Sport T-Shirt</h3>
+                <p class="price">379.000đ</p>
+            </div>
+
+            <div class="product-item">
+                <img src="/shopping-website-php/upload/Ao321.png" alt="">
+                <h3>Áo thể thao HKT - Sport T-Shirt</h3>
+                <p class="price">379.000đ</p>
+            </div>
+
+            <div class="product-item">
+                <img src="/shopping-website-php/upload/Ao321.png" alt="">
+                <h3>Áo thể thao HKT - Sport T-Shirt</h3>
+                <p class="price">379.000đ</p>
+            </div>
+
+            <div class="product-item">
+                <img src="/shopping-website-php/upload/Ao321.png" alt="">
+                <h3>Áo thể thao HKT - Sport T-Shirt</h3>
+                <p class="price">379.000đ</p>
+            </div>
+
+            <div class="product-item">
+                <img src="/shopping-website-php/upload/Ao321.png" alt="">
+                <h3>Áo thể thao HKT - Sport T-Shirt</h3>
+                <p class="price">379.000đ</p>
+            </div>
+
+            <div class="product-item">
+                <img src="/shopping-website-php/upload/Ao321.png" alt="">
+                <h3>Áo thể thao HKT - Sport T-Shirt</h3>
+                <p class="price">379.000đ</p>
+            </div>
+
+            <div class="product-item">
+                <img src="/shopping-website-php/upload/Ao321.png" alt="">
+                <h3>Áo thể thao HKT - Sport T-Shirt</h3>
+                <p class="price">379.000đ</p>
+            </div>
+
+            <div class="product-item">
+                <img src="/shopping-website-php/upload/Ao321.png" alt="">
+                <h3>Áo thể thao HKT - Sport T-Shirt</h3>
+                <p class="price">379.000đ</p>
+            </div>
+
+        </section>
+
+        <div class="pagination">
+
+            <div class="previous-page">
+                <a href="#"><i class="fa-solid fa-arrow-left"></i> TRANG TRƯỚC</a>
+            </div>
+
+            <div class="page-numbers">
+                <a class="active" href="#">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#">4</a>
+                <a href="#">5</a>
+            </div>
+
+            <div class="next-page">
+                <a href="#">TRANG SAU <i class="fa-solid fa-arrow-right"></i></a>
+            </div>
+
+        </div>
+
+    </main>
+    <?php include_once('../includes/footer.php'); ?>
+</body>
+</html>
