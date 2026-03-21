e6bc8016fbfffabbe27d6b2b1359bf7248f7ac62
nguyenkhang011204 <nguyenkhang011204@gmail.com>
2026-03-21 10:51:41 +0700

    Refactor sign in page

--- pages/signIn.php
+++ pages/signIn.php
@@ -1,38 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/signIn.css">
-    <title>Document</title>
-</head>
-<body>
-    <?php include_once('../includes/header.php')?>
-    <?php include_once('../includes/navbar.php')?>
-    
-    <main class="content">
-        <h2>ĐĂNG NHẬP</h2>
-        <form action="" class="form-signIn">
-            <div class="input-group">
-                <i class="fa fa-user"></i>
-                <input type="text" placeholder="Tên đăng nhập">
-            </div>
-    
-            <div class="input-group">
-                <i class="fa fa-lock"></i>
-                <input type="password" placeholder="Mật khẩu">
-            </div>
+<?php
+$page_title = "Đăng nhập";
+$page_css = "../assets/css/signIn.css";
+$base_path = "../";
 
-            <div class="option">
-                <a href="#">Quên mật khẩu</a>
-                <a href="#">Đăng ký</a>
-            </div>
+// Start output buffering to capture page content
+ob_start();
+?>
 
-            <div class="btn-SignIn" type="submit">ĐĂNG NHẬP</div>
+<h2>ĐĂNG NHẬP</h2>
+<form action="" class="form-signIn">
+    <div class="input-group">
+        <i class="fa fa-user"></i>
+        <input type="text" placeholder="Tên đăng nhập">
+    </div>
 
-        </form>
-    </main>
+    <div class="input-group">
+        <i class="fa fa-lock"></i>
+        <input type="password" placeholder="Mật khẩu">
+    </div>
 
-    <?php include_once('../includes/footer.php')?>
-</body>
-</html>
+    <div class="option">
+        <a href="#">Quên mật khẩu</a>
+        <a href="#">Đăng ký</a>
+    </div>
+
+    <div class="btn-SignIn" type="submit">ĐĂNG NHẬP</div>
+
+</form>
+
+<?php
+// Capture output and include layout
+$page_content = ob_get_clean();
+include('../includes/layout.php');
+?>
